13f99ac00f227ba18da5293f61f1f32a0d985a09
Mark Dowell <markd4296@gmail.com>
2021-10-19 21:20:08 -0500

    beginning functions

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print "First Module's Name: {}".format(__name__)
+import math
+
+if __name__ == "__main__":
+
+#clean data function
+	def clean_data
+
+
+#balance teams function
+	def balance_teams
+
+
