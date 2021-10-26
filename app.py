cd60b8eb3638d8b45c47450a4111c3156f4211da
Mark Dowell <markd4296@gmail.com>
2021-10-25 23:15:47 -0500

    begin clean data function

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-import math
+import constants
+import copy
 
-if __name__ == "__main__":
 
 #clean data function
-	def clean_data
+def clean_data():
+	#create new list so as not to overwrite 'constants.py'
+	#also change information into usable integers and booleans
+	clean_list = copy.deepcopy(constants.PLAYERS)
+	loop_thru = 0
+	stop_loop = len(constants.PLAYERS)
+	while loop == True:
+		clean_list[loop_thru]['height'] = clean_list[loop_thru]['height'].removesuffix(" inches")
+		clean_list[loop_thru]['height'] = int(clean_list[loop_start]['height'])
+		
+
 
 
 #balance teams function
-	def balance_teams
+def balance_teams
 
+
+if __name__ == "__main__":
 
