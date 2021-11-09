f10177a35ab391f1f41b533f0ae842608e403d18
Mark Dowell <markd4296@gmail.com>
2021-11-08 22:46:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,19 @@ def clean_data():
 	#also change information into usable integers and booleans
 	clean_list = copy.deepcopy(constants.PLAYERS)
 	loop_thru = 0
-	stop_loop = len(constants.PLAYERS)
-	while loop == True:
+	while loop_thru < len(constants.PLAYERS) :
 		clean_list[loop_thru]['height'] = clean_list[loop_thru]['height'].removesuffix(" inches")
 		clean_list[loop_thru]['height'] = int(clean_list[loop_start]['height'])
 		
+		if clean_list[loop_thru]['experience'] == 'YES':
+			clean_list[loop_thru]['experience'] = True
+		else:
+			clean_list[loop_thru]['experience'] = False
+
+		loop_thru += 1
+		
+def clean_data():
+	clean_list = copy.deepcopy(constants.PLAYERS)
 
 
 
