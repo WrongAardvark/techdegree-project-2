687ac766b4fcb63480e3f47692f80f3c419b9db3
Mark Dowell <30960903+WrongAardvark@users.noreply.github.com>
2021-11-09 13:19:41 -0600

    completed balance and clean defs

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ import constants
 from copy import deepcopy
 
 players_list = deepcopy(constants.PLAYERS)
-teams_list = deepcopy(constants.PLAYERS)
+Panthers = []
+Bandits = []
+Warriors = []
+
 
 
 #clean data function
+#cleans height, experience, guardians
 def clean_data():
 	for player in players_list:
 		height = player['height'].split()
@@ -14,8 +18,11 @@ def clean_data():
 			player['experience'] = True
 		if player['experience'] == 'NO':
 			player['experience'] = False
+		player['guardians'] = player['guardians'].split(' and ')
+		
 
-#
+#divide between experienced and inexperienced players
+#divide teams up equally
 def balance_teams():
 	exp_players = []
 	inexp_players= []
@@ -24,12 +31,30 @@ def balance_teams():
 			exp_players.append(player)
 		if player['experience'] == False:
 			inexp_players.append(player)
-	for index, player in enumerate(exp_players):
-		if player
+	for i, player in enumerate(exp_players):
+		if i <= 3:
+			Panthers.append(player)
+		if i > 3 and i <= 6:
+			Bandits.append(player)
+		if i > 6:
+			Warriors.append(player)
+	for i, player in enumerate(inexp_players):
+		if i <= 3:
+			Panthers.append(player)
+		if i > 3 and i <= 6:
+			Bandits.append(player)
+		if i > 6:
+			Warriors.append(player)
+
+
+def additional_stats():
+	
+			
 
 if __name__ == "__main__":
 	clean_data()
 	balance_teams()
-	print(exp_players)
+	print(Panthers)
+
 
 
