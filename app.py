65a02305aff9bf855c46f5b97a513838ae84f8d8
Mark Dowell <markd4296@gmail.com>
2021-11-08 22:53:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,16 +11,14 @@ def clean_data():
 	while loop_thru < len(constants.PLAYERS) :
 		clean_list[loop_thru]['height'] = clean_list[loop_thru]['height'].removesuffix(" inches")
 		clean_list[loop_thru]['height'] = int(clean_list[loop_start]['height'])
-		
+
 		if clean_list[loop_thru]['experience'] == 'YES':
 			clean_list[loop_thru]['experience'] = True
 		else:
 			clean_list[loop_thru]['experience'] = False
 
 		loop_thru += 1
-		
-def clean_data():
-	clean_list = copy.deepcopy(constants.PLAYERS)
+	
 
 
 
