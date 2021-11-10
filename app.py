3b46fe126f5a512102b4168eca5c2784cbd47757
Mark Dowell <30960903+WrongAardvark@users.noreply.github.com>
2021-11-10 13:37:45 -0600

    Final

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ team_list = {
 }
 
 
-
 #clean data function
 #cleans height, experience, guardians
 def clean_data():
@@ -47,11 +46,12 @@ def balance_teams():
 			team_list['Bandits'].append(player)
 		if i > 5:
 			team_list['Warriors'].append(player)
-	
+
 
 def main_menu():
-
-		print('\nWelcome to the Basketball Stats Tool!\n===== MENU =====')
+	run = True
+	while run == True:
+		print('\n-> Basketball Stats Tool <-\n===== MAIN MENU =====')
 		choice_1 = input('\n - Choose from the options bellow... -\n > 1) Display Team Stats\n > 2) Quit\n\nEnter an option:  ')
 		if int(choice_1) == 1:
 			team_name = input('\n - Select from the teams below... -\n > 1) Panthers\n > 2) Bandits\n > 3) Warriors\n\nEnter an option:  ')
@@ -67,32 +67,37 @@ def main_menu():
 			names = []
 			for player in team_list[team_name]:
 				names.append(player['name'])
-			print('\n- Players on team:\n {}'.format(', '.join(names))+'\n\n')
+			print('\n- Players on team:\n {}'.format(', '.join(names))+'\n')
 			choice_3 = input('\n- Choose from the options below...\n > 1) Additional Stats for the {}\n > 2) Return to Menu\n\nEnter an option:  '.format(team_name))
 			# Additional stats number of inexperienced/experienced players, average height of players, guardians of team
 			if int(choice_3) == 1:
-				team_height = int(team_name['height'])
+				team_height = []
+				for player in team_list[team_name]:
+					team_height.append(player['height'])
 				average_height = sum(team_height) / len(team_name)
 				print('\n- Average Height of Team:  {} inches'.format(average_height))
 				exp_players = 0
 				inexp_players = 0
-				for player in team_name:
+				for player in team_list[team_name]:
 					if player['experience'] == True:
 						exp_players += 1
 					else:
 						inexp_players +=1
 				print('- There are {} experienced players and {} inexperienced players on this team.'.format(exp_players, inexp_players))
 				print('- The guardians on this team are as follows:\n')
-				for player in team_name:
-    					print('\nPlayer: {} Guardian(s): {}'.format(team_name['name'],team_name['guardians']))
-		else:
+				for player in team_list[team_name]:
+    					print(' Player: {} | Guardian(s): {}'.format(player['name'], ', '.join(player['guardians'])))
+			else:
+				continue
+
+		elif int(choice_1) == 2:
 			print('\n\nThanks for using the Basketball Stats Tool!\n')
+			run = False
+
+		else:
+			print("\nPlease enter a valid option...")
 
 
-			
-
-
-			
 
 if __name__ == "__main__":
 	clean_data()
