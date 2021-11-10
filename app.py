5b56ccc16e73de0769a58b04d44666df7d160772
Mark Dowell <markd4296@gmail.com>
2021-11-09 18:39:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -47,36 +47,47 @@ def balance_teams():
 			team_list['Bandits'].append(player)
 		if i > 5:
 			team_list['Warriors'].append(player)
-
-
-
-def additional_stats(team_name):
-	average_height = sum(team_list[team_name['height']]) / len(team_list[team_name['height']])
-	print('Average Height of Team:  {} inches'.format(average_height))
-
-
 	
+
 def main_menu():
-	print('\nWelcome to the Basketball Stats Tool!\n===== MENU =====')
-	choice_1 = input('\n - Choose from the options bellow... -\n > 1) Display Team Stats\n > 2) Quit\n\nEnter an option:  ')
-	if choice_1 == 1:
-		team_name = input('\n - Select from the teams below... -\n > 1) Panthers\n > 2) Bandits\n > 3) Warriors\n\nEnter an option:  ') - 1
-		if team_name == 0:
-			team_name = 'Panthers'
-		if team_name == 1:
-			team_name = 'Bandits'
-		if team_name == 2:
-			team_name = 'Warriors'
-		print('\n --- {} ---: \n----------'.format(team_name))
-		print('\n- Total players: {}'.format(len(team_list[team_name])))
-		names = []
-		for player in team_list[team_name]:
-			names.append(player['name'])
-		print('\n- Players on team:\n {}'.format(', '.join(names))+'\n\n')
-		choice_3 = input('\n- Choose from the options below...\n > 1) Additional Stats for the {}\n > 2) Return to Menu\n\nEnter an option:  '.format(team_name))
-		# Additional stats number of inexperienced/experienced players, average height of players, guardians of team
-		if choice_3 == 1:
-			additional_stats(team_name)
+
+		print('\nWelcome to the Basketball Stats Tool!\n===== MENU =====')
+		choice_1 = input('\n - Choose from the options bellow... -\n > 1) Display Team Stats\n > 2) Quit\n\nEnter an option:  ')
+		if int(choice_1) == 1:
+			team_name = input('\n - Select from the teams below... -\n > 1) Panthers\n > 2) Bandits\n > 3) Warriors\n\nEnter an option:  ')
+			team_name = int(team_name)-1
+			if team_name == 0:
+				team_name = 'Panthers'
+			if team_name == 1:
+				team_name = 'Bandits'
+			if team_name == 2:
+				team_name = 'Warriors'
+			print('\n --- {} ---: \n----------'.format(team_name))
+			print('\n- Total players: {}'.format(len(team_list[team_name])))
+			names = []
+			for player in team_list[team_name]:
+				names.append(player['name'])
+			print('\n- Players on team:\n {}'.format(', '.join(names))+'\n\n')
+			choice_3 = input('\n- Choose from the options below...\n > 1) Additional Stats for the {}\n > 2) Return to Menu\n\nEnter an option:  '.format(team_name))
+			# Additional stats number of inexperienced/experienced players, average height of players, guardians of team
+			if int(choice_3) == 1:
+				team_height = int(team_name['height'])
+				average_height = sum(team_height) / len(team_name)
+				print('\n- Average Height of Team:  {} inches'.format(average_height))
+				exp_players = 0
+				inexp_players = 0
+				for player in team_name:
+					if player['experience'] == True:
+						exp_players += 1
+					else:
+						inexp_players +=1
+				print('- There are {} experienced players and {} inexperienced players on this team.'.format(exp_players, inexp_players))
+				print('- The guardians on this team are as follows:\n')
+				for player in team_name:
+    					print('\nPlayer: {} Guardian(s): {}'.format(team_name['name'],team_name['guardians']))
+		else:
+			print('\n\nThanks for using the Basketball Stats Tool!\n')
+
 
 			
 
